ac6fae6c5c2c033bd2a9092ec1eda81e11261086
HP <mdzaid1299@gmail.com>
2022-09-30 14:16:56 +0530

    firstdone

--- p1-find-the-largest-number/p1-submission.js
+++ p1-find-the-largest-number/p1-submission.js
@@ -5,13 +5,24 @@ let number3 = 45;
 let max;
 
 // Using nested-if
+if(number1 > number2){
+    if(number1 > number3){
+        max = number1;
+    }else{
+        max = number3;
 
+        
+    }
+}else{
+    max = number2;
+    
+
+}
 
 
 console.log(`Largest Number is ${max}`);
 
 // Using ternary operator
-
- 
+(number1>number2)?(number1>number3)?max=number1:max=number3:max=number2;
 
 console.log(`Largest Number is ${max}`);
